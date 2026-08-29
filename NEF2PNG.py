100b869d1f445badb3d3195e4672e0fea5e303df
alx3416 <63263335+alx3416@users.noreply.github.com>
2026-08-29 02:56:21 -0600

    PNG data 3

--- NEF2PNG.py
+++ NEF2PNG.py
@@ -7,8 +7,8 @@ import rawpy
 import imageio.v3 as iio
 
 # --- Configuración ---
-PATH_ENTRADA = r"data\101D3300"      # directorio con los archivos NEF
-PATH_SALIDA = r"data\101D3300-PNG"     # directorio de salida
+PATH_ENTRADA = r"data\100D3300"      # directorio con los archivos NEF
+PATH_SALIDA = r"data\100D3300-PNG"     # directorio de salida
 BITS = 8                            # 8 o 16
 SOBRESCRIBIR = False               # True para sobrescribir PNG existentes
 
